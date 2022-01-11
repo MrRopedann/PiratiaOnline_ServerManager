7218ab9515b764dfacb29706f22103a59882cd6e
Mr Ropedann <48921349+MrRopedann@users.noreply.github.com>
2022-01-11 21:34:28 +0500

    Основные панели
    Добавил основные панели

--- PiratiaServerManager/FormMain.Designer.cs
+++ PiratiaServerManager/FormMain.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.leftMenu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // leftMenu
+            // 
+            this.leftMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(61)))), ((int)(((byte)(66)))));
+            this.leftMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftMenu.Location = new System.Drawing.Point(0, 0);
+            this.leftMenu.Name = "leftMenu";
+            this.leftMenu.Size = new System.Drawing.Size(200, 648);
+            this.leftMenu.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(200, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1233, 648);
+            this.panel1.TabIndex = 1;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1433, 648);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.leftMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FormMain";
             this.Text = "Piratia Online - Server Manager";
             this.ResumeLayout(false);
@@ -42,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel leftMenu;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
