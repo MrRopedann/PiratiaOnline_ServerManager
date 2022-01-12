a81863479b9e507643139c26fd7c823fffb876ba
Mr Ropedann <48921349+MrRopedann@users.noreply.github.com>
2022-01-12 14:18:55 +0500

    Add HideMenu
    Добавил возможность скрывать левое меню

--- PiratiaServerManager/FormMain.cs
+++ PiratiaServerManager/FormMain.cs
@@ -12,9 +12,31 @@ namespace PiratiaServerManager
 {
     public partial class FormMain : Form
     {
+        bool hideMenu = true;
+        string buttonText = "";
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (hideMenu)
+            {
+                leftMenu.Width = 50;
+                buttonText = button1.Text;
+                button1.Text = "";
+                pictureBox1.Image = Properties.Resources.pkodevlogo_min;
+                hideMenu = false;
+            }
+            else
+            {
+                leftMenu.Width = 270;
+                button1.Text = buttonText;
+                buttonText = "";
+                pictureBox1.Image = Properties.Resources.pkodevlogo_full_png_9c0b058fcf0ebbfd09e443f272c3d328;
+                hideMenu = true;
+            }
         }
     }
 }
